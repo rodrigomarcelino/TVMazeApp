22decfb3a1d5e6378a66ab9d63e1719aa1f815f3
rodrigomarcelino <rodrigo.a.marcelino@gmail.com>
2022-02-05 13:21:51 -0300

    create project

---
+++ TVMazeApp/TVMazeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TVMazeApp
+//
+//  Created by Digao on 05/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
